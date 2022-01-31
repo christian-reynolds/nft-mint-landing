4db2cbce99568b84f05266cb748ebae5179ef9f6
christian-reynolds <93774940+christian-reynolds@users.noreply.github.com>
2022-01-31 11:05:20 -0500

    Moved the onScroll to the Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,52 @@
+import { useEffect } from "react";
+
 function Header() {
+    useEffect(() => {
+        const onScroll: EventListener = (event: Event) => {
+            console.log('event: ', window.scrollY);
+
+            let scrollpos = window.scrollY;            
+            const header = document.getElementById("header");
+            const navcontent = document.getElementById("nav-content");
+            const navaction = document.getElementById("navAction");
+            const toToggle = document.querySelectorAll(".toggleColour");
+        
+            if (scrollpos > 10) {
+                header!.classList.add("bg-white");
+                navaction!.classList.remove("bg-white");
+                navaction!.classList.add("gradient");
+                navaction!.classList.remove("text-gray-800");
+                navaction!.classList.add("text-white");
+                //Use to switch toggleColour colours
+                for (let i = 0; i < toToggle.length; i++) {
+                    toToggle[i].classList.add("text-gray-800");
+                    toToggle[i].classList.remove("text-white");
+                }
+                header!.classList.add("shadow");
+                navcontent!.classList.remove("bg-gray-100");
+                navcontent!.classList.add("bg-white");
+            } else {
+                header!.classList.remove("bg-white");
+                navaction!.classList.remove("gradient");
+                navaction!.classList.add("bg-white");
+                navaction!.classList.remove("text-white");
+                navaction!.classList.add("text-gray-800");
+                //Use to switch toggleColour colours
+                for (let i = 0; i < toToggle.length; i++) {
+                    toToggle[i].classList.add("text-white");
+                    toToggle[i].classList.remove("text-gray-800");
+                }
+
+                header!.classList.remove("shadow");
+                navcontent!.classList.remove("bg-white");
+                navcontent!.classList.add("bg-gray-100");
+            }
+        };
+        window.addEventListener("scroll", onScroll);
+
+        return () => window.removeEventListener("scroll", onScroll);
+    }, []);
+      
     return (
         <nav id="header" className="fixed w-full z-30 top-0 text-white">
             <div className="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 py-2">
