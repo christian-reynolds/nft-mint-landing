c4126abbd5ada2efbc9e411d401b1c701eee82d4
christian-reynolds <93774940+christian-reynolds@users.noreply.github.com>
2022-01-31 11:00:31 -0500

    Added onScroll for the header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -24,23 +24,23 @@ function Header() {
                 </button>
                 </div>
                 <div className="w-full flex-grow lg:flex lg:items-center lg:w-auto hidden mt-2 lg:mt-0 bg-white lg:bg-transparent text-black p-4 lg:p-0 z-20" id="nav-content">
-                <ul className="list-reset lg:flex justify-end flex-1 items-center">
-                    <li className="mr-3">
-                    <a className="inline-block py-2 px-4 text-black font-bold no-underline" href="#">Active</a>
-                    </li>
-                    <li className="mr-3">
-                    <a className="inline-block text-black no-underline hover:text-gray-800 hover:text-underline py-2 px-4" href="#">link</a>
-                    </li>
-                    <li className="mr-3">
-                    <a className="inline-block text-black no-underline hover:text-gray-800 hover:text-underline py-2 px-4" href="#">link</a>
-                    </li>
-                </ul>
-                <button
-                    id="navAction"
-                    className="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full mt-4 lg:mt-0 py-4 px-8 shadow opacity-75 focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out"
-                >
-                    Action
-                </button>
+                    <ul className="list-reset lg:flex justify-end flex-1 items-center">
+                        <li className="mr-3">
+                        <a className="inline-block py-2 px-4 text-black font-bold no-underline" href="#">Active</a>
+                        </li>
+                        <li className="mr-3">
+                        <a className="inline-block text-black no-underline hover:text-gray-800 hover:text-underline py-2 px-4" href="#">link</a>
+                        </li>
+                        <li className="mr-3">
+                        <a className="inline-block text-black no-underline hover:text-gray-800 hover:text-underline py-2 px-4" href="#">link</a>
+                        </li>
+                    </ul>
+                    <button
+                        id="navAction"
+                        className="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full mt-4 lg:mt-0 py-4 px-8 shadow opacity-75 focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out"
+                    >
+                        Action
+                    </button>
                 </div>
             </div>
             <hr className="border-b border-gray-100 opacity-25 my-0 py-0" />
